55d9b30dcf4abfb998b6da75c2ce63048fbb2cea
Emad Alhamou <emadalhamo@gmail.com>
2019-03-30 23:28:21 +0100

    with nodemon

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) =>{
-    res.send('Hallo World!');
+    res.send('Hallo World! OK!!!');
 });
 
-app.listen(9000, ()=>{
-    console.log('App run on Port 9000');
+app.listen(4500, ()=>{
+    console.log('App run on Port 4500');
 });
